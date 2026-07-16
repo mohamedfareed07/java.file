62379b98ca0e6c59271b5cf7c2c4607701a48972
mohamedfareed07 <mohamedfareed0786@gmail.com>
2026-07-16 08:45:20 +0530

    Add files via upload

---
+++ java.file/shufflearray.java
@@ -0,0 +1,10 @@
+class shufflearray {
+    public int[] shuffle(int[] nums, int n) {
+        int shuffled[] = new int[nums.length];
+        for (int i = 0; i < n; i++) {
+            shuffled[2 * i] = nums[i];
+            shuffled[2 * i + 1] = nums[n + i];
+        }
+    return shuffled;
+    }
+}
